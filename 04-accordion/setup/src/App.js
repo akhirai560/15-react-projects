18e5c95b021c318895afa17b39fc7b929b4283b2
akhirai560 <akhileshgzp500@gmail.com>
2021-11-29 15:15:02 +0530

    added accordin with single open feature

--- 04-accordion/setup/src/App.js
+++ 04-accordion/setup/src/App.js
@@ -1,8 +1,53 @@
-import React, { useState } from 'react';
-import data from './data';
-import SingleQuestion from './Question';
+import React, { useState, useCallback, useEffect } from "react";
+import data from "./data";
+import SingleQuestion from "./Question";
 function App() {
-  return <h2>accordion project setup</h2>;
+  const [questions, setQuestions] = useState(data);
+  const [openedMap, setOpenedMap] = useState({});
+
+  useEffect(() => {
+    questions.forEach((question, index) => {
+      setOpenedMap((obj) => {
+        return { ...obj, [index]: false };
+      });
+    });
+  }, [questions]);
+
+  const updateOpenedMapOnToggle = useCallback((index, state) => {
+    Object.keys(openedMap).forEach((key) => {
+      if (openedMap[key] === true) {
+        setOpenedMap((obj) => {
+          return { ...obj, [key]: false };
+        });
+      }
+    });
+    setOpenedMap((obj) => {
+      return { ...obj, [index]: state };
+    });
+  });
+
+  return (
+    <main>
+      <div
+        className="container"
+        style={{ display: "flex", flexDirection: "column" }}
+      >
+        <h3>Questions and answers about login</h3>
+        <section className="info">
+          {questions.map((question) => {
+            return (
+              <SingleQuestion
+                key={question.id}
+                {...question}
+                showInfo={openedMap[question.id]}
+                onQuestionToggle={updateOpenedMapOnToggle}
+              />
+            );
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App;
