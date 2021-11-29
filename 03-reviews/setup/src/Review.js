984615a9f3c668eb4b1a544de4fd59912c68d28d
akhirai560 <akhileshgzp500@gmail.com>
2021-11-29 10:33:22 +0530

    completed reviews project

--- 03-reviews/setup/src/Review.js
+++ 03-reviews/setup/src/Review.js
@@ -1,9 +1,69 @@
-import React, { useState } from 'react';
-import people from './data';
-import { FaChevronLeft, FaChevronRight, FaQuoteRight } from 'react-icons/fa';
+import React, { useState } from "react";
+import peoples from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const Review = () => {
-  return <h2>review component</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = peoples[index];
+
+  const nextPerson = () => {
+    setIndex((index) => {
+      let newIndex = index + 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const prevPerson = () => {
+    setIndex((index) => {
+      let newIndex = index - 1;
+      return checkNumber(newIndex);
+    });
+  };
+
+  const checkNumber = (num) => {
+    if (num > peoples.length - 1) {
+      return 0;
+    }
+    if (num < 0) {
+      return peoples.length - 1;
+    }
+    return num;
+  };
+
+  const randomPerson = () => {
+    const newIndex = Math.floor(Math.random() * peoples.length);
+    setIndex((index) => {
+      if (newIndex === index) {
+        return checkNumber(newIndex + 1);
+      }
+
+      return newIndex;
+    });
+  };
+  return (
+    <article className="review">
+      <div className="img-container">
+        <img src={image} alt={name} className="person-img" />
+        <span className="quote-icon">
+          <FaQuoteRight />
+        </span>
+      </div>
+      <h4 className="author">{name}</h4>
+      <p className="job">{job}</p>
+      <p className="info">{text}</p>
+      <div className="button-container">
+        <button className="prev-btn" onClick={() => prevPerson()}>
+          <FaChevronLeft />
+        </button>
+        <button className="next-btn" onClick={() => nextPerson()}>
+          <FaChevronRight />
+        </button>
+        <button className="random-btn" onClick={() => randomPerson()}>
+          suprise me
+        </button>
+      </div>
+    </article>
+  );
 };
 
 export default Review;
--- README.md
+++ README.md
@@ -1,19 +1,5 @@
 # React Projects
 
-#### Support
-
-Find the Content Useful? [You can always buy me a coffee](https://www.buymeacoffee.com/johnsmilga)
-
-#### Tutorial Video
-
-(React Tutorial)[https://youtu.be/iZhV0bILFb0]
-
-#### All Projects
-
-[Projects Site](https://react-projects.netlify.app/)
-
-## Corresponding Tutorial Topics
-
 #### useState
 
 1. Birthday Reminder
@@ -49,16 +35,3 @@ Find the Content Useful? [You can always buy me a coffee](https://www.buymeacoff
 #### React Router and useCallback
 
 15. Cocktails
-
-## Advanced Projects (Course Exclusive)
-
-[Course Link](https://www.udemy.com/course/react-tutorial-and-projects-course/?couponCode=REACT-OCT)
-
-16. Markdown Preview
-17. Random Person
-18. Pagination
-19. Stock Photos
-20. Dark Mode
-21. Movie DB
-22. Hacker News
-23. Quiz
